a5d90ab0ab7c95122f41784f729d05c1cc3964e2
student <student@DESKTOP-A3K4NNP>
2019-03-11 11:56:11 +0500

    Добавьте файлы проекта.

---
+++ Hello/Controllers/MyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hello.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Any()
+        {
+            return Content("Hello,world!");
+        }
+        public ActionResult Some()
+        {
+            return View();
+        }
+    }
+}
